a8b347f2340ac7f3b784c62fcd72f7622df5dcae
Abel Menezes <abelsmenezes@gmail.com>
2022-04-05 00:04:03 -0300

    games app

--- games/models.py
+++ games/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Games(models.Model):
+    name = models.CharField(max_length=13)
+    year = models.IntegerField()
+    season = models.CharField(max_length=6)
+    city = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
--- games/views.py
+++ games/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import GamesSerializer
+from .models import Games
+
+
+# ViewSets define the view behavior.
+class GamesViewSet(viewsets.ModelViewSet):
+    queryset = Games.objects.all()
+    serializer_class = GamesSerializer
 
 # Create your views here.
