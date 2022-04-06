a8b06ba2c78d930153bb4ffdbcdb7e2d65190c78
Abel Menezes <abelsmenezes@gmail.com>
2022-04-06 18:55:44 -0300

    update script

--- apicelero/settings.py
+++ apicelero/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'noc',
     'games',
     'event',
@@ -143,5 +144,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
