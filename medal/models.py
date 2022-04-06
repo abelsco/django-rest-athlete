b2264e1f1d15b7ae587fa28d59f0d3eec5813cc3
Abel Menezes <abelsmenezes@gmail.com>
2022-04-06 18:55:04 -0300

    update app medal

--- medal/models.py
+++ medal/models.py
@@ -1,14 +1,16 @@
-from tkinter import CASCADE
 from django.db import models
 from event.models import Event
 from athlete.models import Athlete
 from games.models import Games
+from django.utils.timezone import datetime
 
 class Medal(models.Model):
     athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, related_name='event_slug', to_field='slug', on_delete=models.CASCADE, default='')
     games = models.ForeignKey(Games, related_name='games_slug', to_field='slug', on_delete=models.CASCADE, default='')
     medal = models.CharField(max_length=6, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
 
     class Meta:
         ordering = ('-id',)
