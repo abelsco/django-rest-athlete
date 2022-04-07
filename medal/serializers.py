6dfa074660f344d99013760b3d55e1acb70c6460
Abel Menezes <abelsmenezes@gmail.com>
2022-04-07 00:18:10 -0300

    update medal serializers

--- medal/serializers.py
+++ medal/serializers.py
@@ -1,7 +1,7 @@
 from .models import Medal
 from rest_framework import serializers
 
-class MedalSerializer(serializers.HyperlinkedModelSerializer):
+class MedalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medal
-        fields = ['athlete', 'games', 'event', 'medal']
+        fields = '__all__'
