25ff44320466c88f926c8e2a718eee4379b73212
Abel Menezes <abelsmenezes@gmail.com>
2022-04-07 00:16:23 -0300

    remove unused

--- runserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /code
-
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
-python manage.py runserver 0.0.0.0:8080
