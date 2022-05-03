c5002dbc8a394939b463fe50cfd24b946762ca70
Abel Menezes <abelsmenezes@gmail.com>
2022-05-03 20:55:39 -0300

    padronizando o populatedb

--- populatedb.py
+++ populatedb.py
@@ -1,12 +1,17 @@
+"""_summary_
+
+    Returns:
+        _type_: _description_
+    """
 import time
 import pandas
 import psycopg2
 from slugify import slugify
 from django.utils.timezone import datetime
 
-conn_db = 'postgres://postgres:postgres@db/postgres'
+CONN_DB = 'postgres://postgres:postgres@db/postgres'
 
-db = psycopg2.connect(conn_db)
+db = psycopg2.connect(CONN_DB)
 
 dfAthlete = pandas.read_csv('dataset/athlete_events.csv')
 dfNocs = pandas.read_csv('dataset/noc_regions.csv', keep_default_na=False)
@@ -18,124 +23,165 @@ files_formated = {
     'events': 'dataset/formated/events.csv',
     'medals': 'dataset/formated/medals.csv',
 }
-    
-    
 
-def normalizeDataFrame():
+
+def normalize_data_frame():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
 
-    nocs = dfNocs.loc[:, ['NOC', 'region', 'notes']].drop_duplicates(subset=['NOC'])
+    nocs = dfNocs.loc[:, ['NOC', 'region', 'notes']].drop_duplicates(subset=[
+                                                                     'NOC'])
     nocs['created'] = datetime.now()
     nocs['updated'] = nocs['created']
     nocs.to_csv(files_formated['nocs'], index=False)
 
-    athletes = dfAthlete.loc[:, ['ID', 'Name', 'Sex', 'Age', 'Height', 'Weight', 'Team', 'NOC']].drop_duplicates(subset=['ID'])
+    athletes = dfAthlete.loc[:, ['ID', 'Name', 'Sex', 'Age', 'Height',
+                                 'Weight', 'Team', 'NOC']].drop_duplicates(subset=['ID'])
     athletes['created'] = datetime.now()
     athletes['updated'] = athletes['created']
     athletes.to_csv(files_formated['athletes'], index=False)
-    
-    games = dfAthlete.loc[:, ['Year', 'Season', 'City', 'Games']].drop_duplicates(subset=['Games'])
+
+    games = dfAthlete.loc[:, ['Year', 'Season', 'City',
+                              'Games']].drop_duplicates(subset=['Games'])
     games['Slug'] = games.Games.apply(slugify)
     games['created'] = datetime.now()
     games['updated'] = games['created']
     games.to_csv(files_formated['games'], index=False)
-    
-    events = dfAthlete.loc[:, ['Event', 'Sport']].drop_duplicates(subset=['Event'])
+
+    events = dfAthlete.loc[:, ['Event', 'Sport']].drop_duplicates(subset=[
+                                                                  'Event'])
     events['Slug'] = events.Event.apply(slugify)
     events['created'] = datetime.now()
     events['updated'] = events['created']
     events.to_csv(files_formated['events'], index=False)
-    
+
     medals = dfAthlete.loc[:, ['Medal', 'ID', 'Event', 'Games']]
     medals['Event'] = medals.Event.apply(slugify)
     medals['Games'] = medals.Games.apply(slugify)
     medals['created'] = datetime.now()
     medals['updated'] = medals['created']
     medals.to_csv(files_formated['medals'], index=False)
-    
+
     # print(athletes)
     # print(games)
     # print(events)
     # print(medals)
 
-    return "Time for Setup: {}".format(time.time() - setup_timer)
+    return f"Time for Setup: {time.time() - setup_timer}"
 
-def createNoc():
+
+def create_noc():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
     cursor = db.cursor()
     cursor.execute('TRUNCATE TABLE noc_noc RESTART IDENTITY CASCADE')
-    sql = '''
+    sql = f'''
     COPY noc_noc (name, region, notes, created, updated)
-    FROM '{}'
+    FROM '/{files_formated['nocs']}'
     DELIMITER ',' CSV header;
-    '''.format('/'+files_formated['nocs'])
+    '''
     cursor.execute(sql)
     db.commit()
     cursor.close()
 
-    return "Time for Copy Nations: {}".format(time.time() - setup_timer)
+    return f"Time for Copy Nations: {time.time() - setup_timer}"
 
-def createAthlete():
+
+def create_athlete():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
     cursor = db.cursor()
     cursor.execute('TRUNCATE TABLE athlete_athlete RESTART IDENTITY CASCADE')
-    sql = '''
+    sql = f'''
     COPY athlete_athlete (id, name, sex, age, height, weight, team, noc_id, created, updated)
-    FROM '{}'
+    FROM '/{files_formated['athletes']}'
     DELIMITER ',' CSV header;
-    '''.format('/'+files_formated['athletes'])
+    '''
     cursor.execute(sql)
     db.commit()
     cursor.close()
 
-    return "Time for Copy Athlete: {}".format(time.time() - setup_timer)
+    return f"Time for Copy Athlete: {time.time() - setup_timer}"
 
-def createGames():
+
+def create_games():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
     cursor = db.cursor()
     cursor.execute('TRUNCATE TABLE games_games RESTART IDENTITY CASCADE')
-    sql = '''
+    sql = f'''
     COPY games_games (year, season, city, name, slug, created, updated)
-    FROM '{}'
+    FROM '/{files_formated['games']}'
     DELIMITER ',' CSV header;
-    '''.format('/'+files_formated['games'])
+    '''
     cursor.execute(sql)
     db.commit()
     cursor.close()
 
-    return "Time for Copy Games: {}".format(time.time() - setup_timer)
+    return f"Time for Copy Games: {time.time() - setup_timer}"
 
-def createEvents():
+
+def create_events():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
     cursor = db.cursor()
     cursor.execute('TRUNCATE TABLE event_event RESTART IDENTITY CASCADE')
-    sql = '''
+    sql = f'''
     COPY event_event (name, sport, slug, created, updated)
-    FROM '{}'
+    FROM '/{files_formated['events']}'
     DELIMITER ',' CSV header;
-    '''.format('/'+files_formated['events'])
+    '''
     cursor.execute(sql)
     db.commit()
     cursor.close()
 
-    return "Time for Copy Events: {}".format(time.time() - setup_timer)
+    return f"Time for Copy Events: {time.time() - setup_timer}"
 
-def createMedals():
+
+def create_medals():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     setup_timer = time.time()
     cursor = db.cursor()
     cursor.execute('TRUNCATE TABLE medal_medal RESTART IDENTITY CASCADE')
-    sql = '''
+    sql = f'''
     COPY medal_medal (medal, athlete_id, event_id, games_id, created, updated)
-    FROM '{}'
+    FROM '/{files_formated['medals']}'
     DELIMITER ',' CSV header;
-    '''.format('/'+files_formated['medals'])
+    '''
     cursor.execute(sql)
     db.commit()
     cursor.close()
 
-    return "Time for Copy Medals: {}".format(time.time() - setup_timer)
+    return f"Time for Copy Medals: {time.time() - setup_timer}"
 
-def syncAthleteNextValSequence():
+
+def sync_athlete_next_val_sequence():
+    """_summary_
+    """
     sql = '''
     BEGIN;
     LOCK TABLE athlete_athlete IN EXCLUSIVE MODE;
@@ -146,21 +192,23 @@ def syncAthleteNextValSequence():
     db.commit()
     cursor.close()
 
+
 def __main__():
-    
+
     setup_timer = time.time()
-    print(normalizeDataFrame())
-    print(createNoc())
-    print(createGames())
-    print(createAthlete())
-    print(createEvents())
-    print(createMedals())
-    syncAthleteNextValSequence()
-    print("Total time: {}".format(time.time() - setup_timer))
+    print(normalize_data_frame())
+    print(create_noc())
+    print(create_games())
+    print(create_athlete())
+    print(create_events())
+    print(create_medals())
+    sync_athlete_next_val_sequence()
+    print(f"Total time: {time.time() - setup_timer}")
+
 
 if __name__ == "__main__":
     __main__()
-    
+
 
 # for row in games.values:
 #     print(row[0:4])
